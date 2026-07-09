070ba44fbf0c0fda792df3d8698c7616998f3fc9
mariama mercy <jaai.massie@gmail.com>
2026-07-08 20:06:43 -0400

    disabled key presses

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -22,6 +24,13 @@ int main(void) {
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q') {
+        if (iscntrl(c)) { //checks if character is control character
+            printf("%d\n", c);
+        } else { //prints if character is a printable character
+            printf("%d ('%c')\n", c, c);
+        }
+        }
     return 0;
 }
+
