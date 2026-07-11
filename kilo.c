eece9d5f46d96ea47b19991def96d6752f7d3fc4
mariama mercy <jaai.massie@gmail.com>
2026-07-10 21:05:01 -0400

    refactoring keyoard input

--- kilo.c
+++ kilo.c
@@ -6,6 +6,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/* defines */
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
 /* data */
 struct termios orig_termios;
 
@@ -19,6 +23,8 @@ void disableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
     die("tcsetattr");
 }
+
+
 void enableRawMode() {
    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode); // from <stdlib.h> calls disableRAWMode() automatially when program exits.
@@ -35,6 +41,30 @@ void enableRawMode() {
 
     
 }
+// WAITS FOR ONE KEYPRESS AND RETURNS IT
+char editorReadKey() {
+  int nread;
+  char c;
+
+  //*inputs*//
+
+  // WAITS FOR A KEYPRESS THEN HANDLES IT
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread ==-1 && errno != EAGAIN) die("read");
+
+  }
+  return c;
+}
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
+}
 
 /* initialization */
 int main(void) {
@@ -42,14 +72,7 @@ int main(void) {
     enableRawMode();
 
     while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
+    editorProcessKeypress();
   }
     return 0;
 }
