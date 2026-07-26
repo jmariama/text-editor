54a152f8babdae8ad563da2b08c2fca4013f7b77
mariama mercy <jmariamercy@gmail.com>
2026-07-26 13:42:18 -0400

    window size function fix

--- kilo.c
+++ kilo.c
@@ -64,19 +64,18 @@ while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
   return c;
 }
 
-int getWindowSize(int *rows, int*cols) {
+int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
-
-  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    editorReadKey();
     return -1;
-
-  } else { 
+  } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
-
     return 0;
   }
- }
+}
 
 
 //draw hearts at beginning of each row
